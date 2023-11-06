f66c2eb71155d19d475aeb92922e7f7a607dc34b
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-11-06 22:54:31 +0000

    Feat: update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,11 +5,6 @@ const Sidebar = () => {
     return (
         <div className={`min-w-[280px] h-full text-[2rem] flex flex-col justify-between pb-4 px-3 bg-[rgba(243,247,255,0.4)]`}>
             <div className="w-full flex flex-col">
-                {/* <section className="w-full mb-8 flex sm:flex-row flex-col sm:mx-3 mx-1 items-center  py-5">
-                    <div className="w-10 h-10 rounded-lg bg-indigo flex items-center justify-center mb-2 mr-2"></div>
-                    <h1 className='font-semibold text-lg'>DIY-FINACE</h1>
-                </section> */}
-
                 <small className='pt-[5rem] ml-3 text-[1rem] font-semibold text-gray-500'>MAIN MENU</small>
                 <section className="flex flex-col w-full font-[300]">
                     <SideItem link={'/'} name={'Home'} icon={
@@ -32,7 +27,6 @@ const Sidebar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
                         </svg>
-
                     } className={`w-full hover:border hover:bg-white hover:text-indigo-600 hover:font-[600] rounded-lg py-3 pl-3`} />
                     <SideItem link={'/'} name={'Settings'} icon={
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -42,11 +36,7 @@ const Sidebar = () => {
 
                     } className={`w-full hover:border hover:bg-white hover:text-indigo-600 hover:font-[600] rounded-lg py-3 pl-3`} />
                 </section>
-
-
             </div>
-
-
         </div>
     )
 }
